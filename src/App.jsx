5dafc127af92161dfadb6559fd67e774b5d15c1b
fabianemcortez <fabianemcortez@gmail.com>
2023-06-07 22:13:39 -0300

    pelo menos fiz um

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,33 @@
 import "./App.css";
+import PostsList from "./components/PostsList";
 
 /* BONUS POINTS PARA QUEM ESTILIZAR MINIMAMENTE OS COMPONENTES!! */
 
 function App() {
-   return (
+  return (
+    <div>
+      <h1>Atividade Fetch</h1>
+
+      <small>
+        Axios já instalado. (olhar as dependencias do arquivo: package.json)
+      </small>
+
+      <p>
+        Nessa atividade vamos treinar buscar as informações de várias API's.
+      </p>
+      <small>Use como exemplo o código da aula. Arquivo: Axios.jsx</small>
+
       <div>
-         <h1>Atividade Fetch</h1>
-
-         <small>
-            Axios já instalado. (olhar as dependencias do arquivo: package.json)
-         </small>
-
-         <p>
-            Nessa atividade vamos treinar buscar as informações de várias API's.
-         </p>
-         <small>Use como exemplo o código da aula. Arquivo: Axios.jsx</small>
-
-         <div>
-         {/* 
+        <PostsList />
+        {/* 
             Coloque aqui embaixo os componentes criados por você
             exemple: 
             
             <PostsList />
          */}
-         </div>
       </div>
-   );
+    </div>
+  );
 }
 
 export default App;
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -3,3 +3,35 @@ END POINT: https://dummyjson.com/posts
 Rendereizar os campos: title, body, reactions
 Não esqueça de criar o botão para mostrar e sumir com a lista (como exemplo em aula)
 */
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function PostsList() {
+  const [postsList, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchPostsList() {
+      const response = await axios.get("https://dummyjson.com/posts");
+      console.log(response);
+      setPosts(response.data.posts);
+    }
+
+    fetchPostsList();
+  }, []);
+
+  return (
+    <div>
+      <div>
+        {postsList.map((post) => (
+          <div key={post.id}>
+            <p>{post.title}</p>
+            <p>{post.body}</p>
+            <p>{post.reactions}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+
